c56a6c9fb8bb59c908d142cdde270cf30210ad34
Ian Kendall <ian.kendall@ultimatescoreboard.com>
2022-03-05 14:35:09 -0800

    capitalization on Dosshoue

--- src/components/home/home.jsx
+++ src/components/home/home.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Home = () => {
   return (
     <div>
-      <h2>Welcome to dosshouse</h2>
+      <h2>Welcome to Dosshouse</h2>
       <p>
         Dosshouse is a combination of personal projects and utilities. Please
         browse around and try it out.
