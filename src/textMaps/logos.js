30c15ac318b2c0b25686c2e94986b7c87f3e4bb6
Ian Kendall <ian.kendall@ultimatescoreboard.com>
2025-12-08 12:15:54 -0800

    64: update floags to open source

--- src/textMaps/logos.js
+++ src/textMaps/logos.js
@@ -1,121 +1,74 @@
+const baseUrl = "https://purecatamphetamine.github.io/country-flag-icons/3x2/";
+
 export default {
-  Algeria:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/7/77/Flag_of_Algeria.svg/330px-Flag_of_Algeria.svg.png",
-  Argentina:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1920px-Flag_of_Argentina.svg.png",
-  ARG: "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1a/Flag_of_Argentina.svg/1920px-Flag_of_Argentina.svg.png",
-  Australia:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/88/Flag_of_Australia_%28converted%29.svg/1024px-Flag_of_Australia_%28converted%29.svg.png",
-  Austria:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/1920px-Flag_of_Austria.svg.png",
-  AUT: "https://upload.wikimedia.org/wikipedia/commons/thumb/4/41/Flag_of_Austria.svg/1920px-Flag_of_Austria.svg.png",
-  Belgium:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Belgium.svg/1024px-Flag_of_Belgium.svg.png",
-  Brazil:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1280px-Flag_of_Brazil.svg.png",
-  BRA: "https://upload.wikimedia.org/wikipedia/en/thumb/0/05/Flag_of_Brazil.svg/1280px-Flag_of_Brazil.svg.png",
-  Cameroon:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/4/4f/Flag_of_Cameroon.svg/1920px-Flag_of_Cameroon.svg.png",
-  Canada:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Canada_%28Pantone%29.svg/1920px-Flag_of_Canada_%28Pantone%29.svg.png",
-  "Cape Verde":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/38/Flag_of_Cape_Verde.svg/330px-Flag_of_Cape_Verde.svg.png",
-  Colombia:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/21/Flag_of_Colombia.svg/330px-Flag_of_Colombia.svg.png",
-  "Costa Rica":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/bc/Flag_of_Costa_Rica_%28state%29.svg/1024px-Flag_of_Costa_Rica_%28state%29.svg.png",
-  Croatia:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/Flag_of_Croatia.svg/1920px-Flag_of_Croatia.svg.png",
-  Curacao:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/b/b1/Flag_of_Cura%C3%A7ao.svg/330px-Flag_of_Cura%C3%A7ao.svg.png",
-  Denmark:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/9/9c/Flag_of_Denmark.svg/1280px-Flag_of_Denmark.svg.png",
-  Ecuador:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/e/e8/Flag_of_Ecuador.svg/1920px-Flag_of_Ecuador.svg.png",
-  Egypt:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/1920px-Flag_of_Egypt.svg.png",
-  EGY: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Egypt.svg/1920px-Flag_of_Egypt.svg.png",
-  England:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/1920px-Flag_of_England.svg.png",
-  ENG: "https://upload.wikimedia.org/wikipedia/en/thumb/b/be/Flag_of_England.svg/1920px-Flag_of_England.svg.png",
-  France:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/800px-Flag_of_France.svg.png?20220120162234",
-  FRA: "https://upload.wikimedia.org/wikipedia/en/thumb/c/c3/Flag_of_France.svg/800px-Flag_of_France.svg.png?20220120162234",
-  Germany:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png",
-  GER: "https://upload.wikimedia.org/wikipedia/en/thumb/b/ba/Flag_of_Germany.svg/1920px-Flag_of_Germany.svg.png",
-  Ghana:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/19/Flag_of_Ghana.svg/1920px-Flag_of_Ghana.svg.png",
-  Haiti:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/56/Flag_of_Haiti.svg/960px-Flag_of_Haiti.svg.png",
-  Italy:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/1920px-Flag_of_Italy.svg.png",
-  ITA: "https://upload.wikimedia.org/wikipedia/en/thumb/0/03/Flag_of_Italy.svg/1920px-Flag_of_Italy.svg.png",
-  Iran: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ca/Flag_of_Iran.svg/1920px-Flag_of_Iran.svg.png",
-  "Ivory Coast":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_C%C3%B4te_d%27Ivoire.svg/330px-Flag_of_C%C3%B4te_d%27Ivoire.svg.png",
-  Japan:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1920px-Flag_of_Japan.svg.png",
-  JAP: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9e/Flag_of_Japan.svg/1920px-Flag_of_Japan.svg.png",
-  Jordan:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/c0/Flag_of_Jordan.svg/330px-Flag_of_Jordan.svg.png",
-  Mexico:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1920px-Flag_of_Mexico.svg.png",
-  MEX: "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fc/Flag_of_Mexico.svg/1920px-Flag_of_Mexico.svg.png",
-  Morocco:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/1920px-Flag_of_Morocco.svg.png",
-  MOR: "https://upload.wikimedia.org/wikipedia/commons/thumb/2/2c/Flag_of_Morocco.svg/1920px-Flag_of_Morocco.svg.png",
-  Netherlands:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/20/Flag_of_the_Netherlands.svg/1920px-Flag_of_the_Netherlands.svg.png",
-  Norway:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d9/Flag_of_Norway.svg/250px-Flag_of_Norway.svg.png",
-  "New Zealand":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_New_Zealand.svg/1920px-Flag_of_New_Zealand.svg.png",
-  NZL: "https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Flag_of_New_Zealand.svg/1920px-Flag_of_New_Zealand.svg.png",
-  Panama:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/ab/Flag_of_Panama.svg/330px-Flag_of_Panama.svg.png",
-  Paraguay:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/2/27/Flag_of_Paraguay.svg/330px-Flag_of_Paraguay.svg.png",
-  Poland:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/1/12/Flag_of_Poland.svg/1920px-Flag_of_Poland.svg.png",
-  Portugal:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1920px-Flag_of_Portugal.svg.png",
-  POR: "https://upload.wikimedia.org/wikipedia/commons/thumb/5/5c/Flag_of_Portugal.svg/1920px-Flag_of_Portugal.svg.png",
-  Qatar:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/6/65/Flag_of_Qatar.svg/1920px-Flag_of_Qatar.svg.png",
-  "Saudi Arabia":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/1920px-Flag_of_Saudi_Arabia.svg.png",
-  SAU: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/0d/Flag_of_Saudi_Arabia.svg/1920px-Flag_of_Saudi_Arabia.svg.png",
-  Senegal:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fd/Flag_of_Senegal.svg/1920px-Flag_of_Senegal.svg.png",
-  Serbia:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/ff/Flag_of_Serbia.svg/1920px-Flag_of_Serbia.svg.png",
-  "South Africa":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/1920px-Flag_of_South_Africa.svg.png",
-  RSA: "https://upload.wikimedia.org/wikipedia/commons/thumb/a/af/Flag_of_South_Africa.svg/1920px-Flag_of_South_Africa.svg.png",
-  "South Korea":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/1920px-Flag_of_South_Korea.svg.png",
-  KOR: "https://upload.wikimedia.org/wikipedia/commons/thumb/0/09/Flag_of_South_Korea.svg/1920px-Flag_of_South_Korea.svg.png",
-  Spain:
-    "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1920px-Flag_of_Spain.svg.png",
-  ESP: "https://upload.wikimedia.org/wikipedia/en/thumb/9/9a/Flag_of_Spain.svg/1920px-Flag_of_Spain.svg.png",
-  Scotland:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/1/10/Flag_of_Scotland.svg/960px-Flag_of_Scotland.svg.png",
-  Switzerland:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/f3/Flag_of_Switzerland.svg/1024px-Flag_of_Switzerland.svg.png",
-  Tunisia:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/1920px-Flag_of_Tunisia.svg.png",
-  TUN: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/ce/Flag_of_Tunisia.svg/1920px-Flag_of_Tunisia.svg.png",
-  "United Arab Emirates":
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/1920px-Flag_of_the_United_Arab_Emirates.svg.png",
-  UAE: "https://upload.wikimedia.org/wikipedia/commons/thumb/c/cb/Flag_of_the_United_Arab_Emirates.svg/1920px-Flag_of_the_United_Arab_Emirates.svg.png",
-  "United States":
-    "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png",
-  USA: "https://upload.wikimedia.org/wikipedia/en/thumb/a/a4/Flag_of_the_United_States.svg/1920px-Flag_of_the_United_States.svg.png",
-  Uruguay:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fe/Flag_of_Uruguay.svg/1920px-Flag_of_Uruguay.svg.png",
-  Uzbekistan:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/8/84/Flag_of_Uzbekistan.svg/960px-Flag_of_Uzbekistan.svg.png",
-  Wales:
-    "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dc/Flag_of_Wales.svg/1024px-Flag_of_Wales.svg.png",
+  Algeria: baseUrl + "DZ.svg",
+  Argentina: baseUrl + "AR.svg",
+  ARG: baseUrl + "AR.svg",
+  Australia: baseUrl + "AU.svg",
+  Austria: baseUrl + "AT.svg",
+  AUT: baseUrl + "AT.svg",
+  Belgium: baseUrl + "BE.svg",
+  Brazil: baseUrl + "BR.svg",
+  BRA: baseUrl + "BR.svg",
+  Cameroon: baseUrl + "CM.svg",
+  Canada: baseUrl + "CA.svg",
+  "Cape Verde": baseUrl + "CV.svg",
+  Colombia: baseUrl + "CO.svg",
+  "Costa Rica": baseUrl + "CR.svg",
+  Croatia: baseUrl + "HR.svg",
+  Curacao: baseUrl + "CW.svg",
+  Denmark: baseUrl + "DK.svg",
+  Ecuador: baseUrl + "EC.svg",
+  Egypt: baseUrl + "EG.svg",
+  EGY: baseUrl + "EG.svg",
+  England: baseUrl + "GB-ENG.svg",
+  ENG: baseUrl + "GB-ENG.svg",
+  France: baseUrl + "FR.svg",
+  FRA: baseUrl + "FR.svg",
+  Germany: baseUrl + "DE.svg",
+  GER: baseUrl + "DE.svg",
+  Ghana: baseUrl + "GH.svg",
+  Haiti: baseUrl + "HT.svg",
+  Italy: baseUrl + "IT.svg",
+  ITA: baseUrl + "IT.svg",
+  Iran: baseUrl + "IR.svg",
+  "Ivory Coast": baseUrl + "CI.svg",
+  Japan: baseUrl + "JP.svg",
+  JAP: baseUrl + "JP.svg",
+  Jordan: baseUrl + "JO.svg",
+  Mexico: baseUrl + "MX.svg",
+  MEX: baseUrl + "MX.svg",
+  Morocco: baseUrl + "MA.svg",
+  MOR: baseUrl + "MA.svg",
+  Netherlands: baseUrl + "NL.svg",
+  Norway: baseUrl + "NO.svg",
+  "New Zealand": baseUrl + "NZ.svg",
+  NZL: baseUrl + "NZ.svg",
+  Panama: baseUrl + "PA.svg",
+  Paraguay: baseUrl + "PY.svg",
+  Poland: baseUrl + "PL.svg",
+  Portugal: baseUrl + "PT.svg",
+  POR: baseUrl + "PT.svg",
+  Qatar: baseUrl + "QA.svg",
+  "Saudi Arabia": baseUrl + "SA.svg",
+  SAU: baseUrl + "SA.svg",
+  Senegal: baseUrl + "SN.svg",
+  Serbia: baseUrl + "RS.svg",
+  "South Africa": baseUrl + "ZA.svg",
+  RSA: baseUrl + "ZA.svg",
+  "South Korea": baseUrl + "KR.svg",
+  KOR: baseUrl + "KR.svg",
+  Spain: baseUrl + "ES.svg",
+  ESP: baseUrl + "ES.svg",
+  Scotland: baseUrl + "GB-SCT.svg",
+  Switzerland: baseUrl + "CH.svg",
+  Tunisia: baseUrl + "TN.svg",
+  TUN: baseUrl + "TN.svg",
+  "United Arab Emirates": baseUrl + "AE.svg",
+  UAE: baseUrl + "AE.svg",
+  "United States": baseUrl + "US.svg",
+  USA: baseUrl + "US.svg",
+  Uruguay: baseUrl + "UY.svg",
+  Uzbekistan: baseUrl + "UZ.svg",
+  Wales: baseUrl + "GB-WLS.svg",
 };
