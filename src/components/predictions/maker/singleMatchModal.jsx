c363388c8e829be83697d8217091cf96a8e5e24c
Ian Kendall <ian.kendall@ultimatescoreboard.com>
2026-04-18 19:49:13 -0700

    update header in match modals

--- src/components/predictions/maker/singleMatchModal.jsx
+++ src/components/predictions/maker/singleMatchModal.jsx
@@ -14,9 +14,9 @@ const SingleMatchModal = ({ isOpen, setIsOpen, match, finalRoundNumber }) => {
   return (
     <BasicModal isOpen={isOpen} onClose={setIsOpen} style={confirmModalStyle}>
       <div className="text-center">
-        <h3>
-          <b>Match Number {match.metadata?.matchNumber || match.matchNumber}</b>
-        </h3>
+        <div className="standout-header">
+          Match Number {match.metadata?.matchNumber || match.matchNumber}
+        </div>
         <p>{translateRound(match.round, finalRoundNumber)}</p>
         <p>
           {match.homeTeamName} vs {match.awayTeamName}
