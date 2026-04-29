fb2a5ad23cef2af96fc019a1022bf30b66a3cd2e
Ian Kendall <ian.kendall@ultimatescoreboard.com>
2026-04-29 10:47:41 -0700

    cleanup dead code and linking

--- src/textMaps/pageTitles.js
+++ src/textMaps/pageTitles.js
@@ -1,9 +1,6 @@
 const titleMap = {
-  // active_sites: "Active Sites",
-  // home: "Home",
   competitions: "Competitions",
   submissions: "Submissions",
-  // spotify_api: "Spotify API",
 };
 
 export default function setPageTitle(pathname) {
